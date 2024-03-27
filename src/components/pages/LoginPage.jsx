20a5210db3bb710a3dc9192f49efe7e3da17ba6a
jauresmanouan <jauresmanouan@gmail.com>
2024-03-27 14:48:33 +0000

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import { useState } from "react"
 
 export default function LoginPage() {
+    // state
+    const [prenom, setPrenom] = useState("Bob")
+
+    // Comportement
+    const handleSubmit = () => { 
+        alert(`Bonjour ${prenom}`)
+     }
+
+    const handleChange = (event) => { 
+        setPrenom(event.target.value)
+     }
+
+    // Affichage
   return (
-    <div>LoginPage</div>
+  <div>
+    <h1>Bienvenue chez nous !</h1>
+    <br />
+    <p>Connectez-vous</p>
+    <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" required placeholder="Entrez votre prénon..." />
+        <button>Accédez à votre espace</button>
+    </form>
+  </div>
   )
 }
