287a8ef7a6cff4cb45817e6d614cfedc80db85dc
jauresmanouan <jauresmanouan@gmail.com>
2024-04-01 01:52:52 +0000

    ✨ F02 : ERROR > Create error page

---
+++ src/components/pages/error/ErrorPage.jsx
@@ -0,0 +1,12 @@
+export default function ErrorPage() {
+    
+  const handleClick = () => {
+    <link rel="stylesheet" href="/" />
+  }
+  return (
+    <div>
+        <h1>ErrorPage</h1>
+        <button onClick={handleClick}>Retouner à la page d'accueil</button>
+    </div>
+  )
+}
