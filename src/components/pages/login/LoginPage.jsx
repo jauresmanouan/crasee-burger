91411f29d5693b19af1a256a8b4b171c7b63af87
jauresmanouan <jauresmanouan@gmail.com>
2024-04-05 08:19:51 +0000

    💄 F03: LOGIN > LoginPage > Done with loginpage style

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -1,8 +1,23 @@
-import LoginForm from "./LoginForm"
+import styled from "styled-components";
+import LoginForm from "./LoginForm";
+import Logo from "../../reusale-ui/Logo";
 
 export default function LoginPage() {
-  
   return (
-    <LoginForm/>
-  )
-} 
+    <LoginFormStyled>
+      <Logo />
+      <LoginForm />
+    </LoginFormStyled>
+  );
+}
+
+const LoginFormStyled = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-size: cover;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url("src/assets/F03-burger-background.jpg");
+`;
