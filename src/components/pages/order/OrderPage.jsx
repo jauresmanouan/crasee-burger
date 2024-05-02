c65738d84002ef26a8bc253ce6196c2515193f51
jauresmanouan <jauresmanouan@gmail.com>
2024-05-02 01:52:47 +0000

    ♻️ F03: LOGIN > OrderPage > Refactor code

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,17 +1,14 @@
 import { Link, useParams } from "react-router-dom";
 
 export default function OrderPage() {
-  const {username} = useParams()
+  const { username } = useParams();
 
   return (
     <div>
       <h1>Bonjour {username}</h1>
       <Link to="/">
         <button>Déconnexion</button>
-      </Link> 
+      </Link>
     </div>
-  )
+  );
 }
-
-
-
