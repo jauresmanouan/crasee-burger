e901837753bc53af500c06462880e76cc872197b
jauresmanouan <jauresmanouan@gmail.com>
2024-05-03 00:01:42 +0000

    ♻️ F03 : LOGIN > LoginForm > Integrate Component TextInput in my code

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
 import { IoChevronForward } from "react-icons/io5";
-import Input from "./Input";
+import TextInput from "./TextInput";
 
 export default function LoginForm() {
   const [prenom, setPrenom] = useState("");
@@ -13,12 +14,21 @@ export default function LoginForm() {
     navigate(`order/${prenom}`);
   };
 
+  const handleChange = (event) => {
+    setPrenom(event.target.value);
+  };
+
   return (
     <LoginFormStyled onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous !</h1>
       <hr />
       <h2>Connectez-vous</h2>
-      <Input/>
+      <TextInput
+        onChange={handleChange}
+        InsertIcon={<BsPersonCircle className="icon" />}
+        placeholder={"Entrez votre prénon..."}
+        required
+      />
       <button className="button-with-icon">
         <span>Accédez à mon espace</span>
         <IoChevronForward className="icon-forward" />
@@ -48,33 +58,7 @@ const LoginFormStyled = styled.form`
     width: 80%;
     border: 1.5px solid #ffa01b;
   }
-
-  .input-with-container {
-    background-color: #fff;
-    border-radius: 5px;
-    display: flex;
-    align-items: center;
-    padding: 18px 24px;
-    margin: 18px 0;
-
-    .icon {
-      font-size: 15px;
-      margin-right: 8px;
-      color: #93a2b1;
-    }
-
-    input {
-      border: none;
-      font-size: 15px;
-      color: #17161a;
-    }
-
-    &::placeholder {
-      background: white;
-      color: lightgray;
-    }
-  }
-
+  
   .button-with-icon {
     width: 100%;
     background-color: #ff9f1b;
