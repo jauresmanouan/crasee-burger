c57c878a7d412a281db6b02d4cbab9aaccd7eea5
jauresmanouan <jauresmanouan@gmail.com>
2024-05-06 13:05:41 +0000

    ✨ F04: ORDER > OrderPage > Add backrground color

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,14 +1,21 @@
 import { Link, useParams } from "react-router-dom";
+import styled from "styled-components";
 
 export default function OrderPage() {
   const { username } = useParams();
 
   return (
-    <div>
+    <OrderPageStyled>
       <h1>Bonjour {username}</h1>
       <Link to="/">
         <button>Déconnexion</button>
       </Link>
-    </div>
+    </OrderPageStyled>
   );
 }
+
+const OrderPageStyled = styled.div`
+  height: 100vh;
+  background-color: #FFA01B;
+;
+`;
