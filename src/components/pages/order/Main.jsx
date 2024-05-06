b7bbd4860cd91deffe827f9d7355e0326f2610ca
jauresmanouan <jauresmanouan@gmail.com>
2024-05-06 13:14:44 +0000

    ✨ F04: ORDER > Add component Main with

---
+++ src/components/pages/order/Main.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export default function Main() {
+  return <MainStyled>Main</MainStyled>;
+}
+const MainStyled = styled.div`
+  background-color: red;
+  width: 1400px;
+  height: 834.7px;
+`;
