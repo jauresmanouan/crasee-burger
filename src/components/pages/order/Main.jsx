75aad5bd89a1dd2401474fbf3854b7a2cc0ac953
jauresmanouan <jauresmanouan@gmail.com>
2024-05-07 00:16:26 +0000

    💄 F04 : ORDER > OrderPAge > Main > Done with ui needed for this ticket

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled>Main</MainStyled>;
+  return <MainStyled></MainStyled>;
 }
 const MainStyled = styled.div`
-  background-color: red;
+  background-color: ${theme.colors.background_white};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   width: 1400px;
   height: 834.7px;
+  border-radius: 0px 0px 15px 15px;
 `;
