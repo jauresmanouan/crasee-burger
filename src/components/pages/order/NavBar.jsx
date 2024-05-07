0391c945a4a048cecc36c962e36ac85f65fd738a
jauresmanouan <jauresmanouan@gmail.com>
2024-05-07 00:12:44 +0000

    💄 F04: ORDER > NavBar > Done with navbar UI but image of logo still doesn't appear

--- src/components/pages/order/NavBar.jsx
+++ src/components/pages/order/NavBar.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import Logo from "../../reusale-ui/Logo";
 import Profile from "./Profile";
+import { theme } from "../../../theme";
 
 export default function NavBar() {
   return (
@@ -13,13 +14,19 @@ export default function NavBar() {
 
 const NavBarStyled = styled.div`
   width: 1400px;
-  background-color: blue;
+  background-color: ${theme.colors.background_white};
   display: flex;
   justify-content: space-between;
   align-items: center;
-  
+  border-top-right-radius: 15px;
+  border-top-left-radius: 15px;
+
+  .navbar {
+    Profile {
+      border: 5px solid red;
+    }
+  }
   .logo {
     margin-left: 20px;
   }
-
 `;
