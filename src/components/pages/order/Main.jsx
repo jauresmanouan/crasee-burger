8c92b24130520aea18eed6eb4a55222f35536c9b
jauresmanouan <jauresmanouan@gmail.com>
2024-05-09 22:19:57 +0000

    🚧 F05: ORDER > Main > Create layout for Menu and Basket. Add also maquette for the final code

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -2,12 +2,29 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="menu">Menu</div>
+      <div className="basket">Basket</div>
+    </MainStyled>
+  );
 }
 const MainStyled = styled.div`
   background-color: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-  width: 1400px;
+  width: 1400px; 
   height: 834.7px;
   border-radius: 0px 0px 15px 15px;
+
+  display: grid;
+  grid-template-columns: 25% 1fr ;
+
+
+  .menu{
+    background-color: red;
+  }
+
+  .basket{
+    background-color: blue;
+  }
 `;
