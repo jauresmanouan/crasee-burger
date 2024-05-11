6f1733943c1f46f221035dbb63789c8eda9d86c2
jauresmanouan <jauresmanouan@gmail.com>
2024-05-11 01:26:31 +0000

    ♻️ F05 : ORDER > MAIN > MENU > Add funtion formatPrice and refactor code

--- src/components/pages/order/Main/Menu/Menu.jsx
+++ src/components/pages/order/Main/Menu/Menu.jsx
@@ -2,18 +2,20 @@ import styled from "styled-components";
 import { fakeMenu2 } from "../../../../../fakeData/fakeMenu";
 import { useState } from "react";
 import Card from "./Card";
+import { formatPrice } from "../../../../../utils/maths";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
 
   return (
     <MenuStyled>
-      {menu.map((produit) => {
+      {menu.map(({id, imageSource, title, price}) => {
         return (
           <Card
-            imageSource={produit.imageSource}
-            title={produit.title}
-            price={produit.price}
+            key={id}
+            imageSource={imageSource}
+            title={title}
+            price={formatPrice(price)}
           />
         );
       })}
@@ -27,5 +29,4 @@ const MenuStyled = styled.div`
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
-
 `;
