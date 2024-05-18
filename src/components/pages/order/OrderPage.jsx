bbc47e19d35a80716e9b13c71623b4128b1f5d21
jauresmanouan <jauresmanouan@gmail.com>
2024-05-18 14:23:29 +0000

    ♻️ F07 : ORDER > MAIN > ADMIN > Use context

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -2,13 +2,24 @@ import styled from "styled-components";
 import Main from "./Main/Main";
 import NavBar from "./NavBar/NavBar";
 import { theme } from "../../../theme";
+import { useState } from "react";
+import OrderContext from "../../../context/OrderContext";
 
 export default function OrderPage() {
+  const [isAdmin, setIsAdmin] = useState(true);
+
+  const orderContextValue = {
+    isAdmin,
+    setIsAdmin,
+  }
+
   return (
-    <OrderPageStyled>
-      <NavBar />
-      <Main />
-    </OrderPageStyled>
+    <OrderContext.Provider value={orderContextValue}>
+      <OrderPageStyled>
+        <NavBar />
+        <Main />
+      </OrderPageStyled>
+    </OrderContext.Provider>
   );
 }
 
