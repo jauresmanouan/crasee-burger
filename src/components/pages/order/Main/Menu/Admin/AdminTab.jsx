8d1902e7c598f40d3c426d38242e537e367e3792
jauresmanouan <jauresmanouan@gmail.com>
2024-05-18 15:32:34 +0000

    ✨ F07 : ORDER > MAIN > ADMIN > Introduce <Tab/>

--- src/components/pages/order/Main/Menu/Admin/AdminTab.jsx
+++ src/components/pages/order/Main/Menu/Admin/AdminTab.jsx
@@ -1,9 +1,19 @@
 import styled from "styled-components";
+import Tab from "../../../../../reusale-ui/Tab";
+import { IoChevronDown } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
+import { RiPencilFill } from "react-icons/ri";
 
 export default function AdminTab() {
-  return <AdminTabStyled>AdminTab</AdminTabStyled>;
+  return (
+    <AdminTabStyled>
+      <Tab icon={<IoChevronDown />} />
+      {/* <Tab icon={<FaPlus />} name={"Ajouter un produit"} />
+      <Tab icon={<RiPencilFill />} name={"Modifier un produit"} /> */}
+    </AdminTabStyled>
+  );
 }
 
 const AdminTabStyled = styled.div`
-  border: 2px solid blue;
+  display: flex;
 `;
---
+++ src/components/reusale-ui/Tab.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export default function Tab({ icon, name }) {
+  return (
+    <TabStyled>
+      <span className="icon">{icon}</span>
+      <span className="name">{name}</span>
+    </TabStyled>
+  );
+}
+
+const TabStyled = styled.button`
+  cursor: pointer;
+  height: 43px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 5px 5px 0px 0px;
+  background: #ffffff;
+
+  padding: 0 22px;
+
+  .icon {
+    width: 16px;
+    height: 16px;
+    color: #93a2b1;
+
+  }
+  .name {
+    font-family: "Open Sans";
+    font-size: 16px;
+    font-weight: 400;
+    color: #93a2b1;
+  }
+`;
