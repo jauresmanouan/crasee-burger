b76ef4fa920454b40967ba6c3772af571e2e77e3
jauresmanouan <jauresmanouan@gmail.com>
2024-06-06 04:39:00 +0000

    ✨ F08 :ADMIN > AjouterProduit > Previsualisation image

--- src/components/pages/order/Main/Menu/Admin/AdminContent/AjouterProduits.jsx
+++ src/components/pages/order/Main/Menu/Admin/AdminContent/AjouterProduits.jsx
@@ -21,33 +21,46 @@ export default function AjouterProduits() {
     const newProductToAdd = {
       ...newProduct,
       id: crypto.randomUUID(),
-    }
+    };
     handleAdd(newProductToAdd);
   };
 
   const handleChange = (event) => {
-    const {name, value} = event.target
-    setNewProduct({ ...newProduct, [name]:value });
+    const { name, value } = event.target;
+    setNewProduct({ ...newProduct, [name]: value });
   };
 
   // Affichage
   return (
     <AjouterProduitsStyled>
-      <div className="image-produit">Image produit</div>
+      <div className="image-produit">
+        {newProduct.imageSource ? (
+          <img
+            className="presence-image"
+            src={newProduct.imageSource}
+            alt={newProduct.title}
+          />
+        ) : (
+          <div className="aucune-image">Aucune image</div>
+        )}
+      </div>
       <div className="infos-produit">
-        <input name="title"
+        <input
+          name="title"
           type="text"
           value={newProduct.title}
           onChange={handleChange}
           placeholder="Nom du produit (ex: Super Burger)"
         />
-        <input name="imageSource"
+        <input
+          name="imageSource"
           type="url"
           value={newProduct.imageSource}
           onChange={handleChange}
           placeholder="Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
         />
-        <input name="price"
+        <input
+          name="price"
           type="number"
           onChange={handleChange}
           value={newProduct.price}
@@ -75,6 +88,22 @@ const AjouterProduitsStyled = styled.div`
   .image-produit {
     background-color: yellow;
     grid-area: 1/1/4/2;
+
+    .presence-image {
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+      object-position: center;
+    }
+
+    .aucune-image {
+      background-color: blueviolet;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 100%;
+      height: 100%;
+    }
   }
 
   .infos-produit {
