d63ce6c9f5276893d99acc4ade5b655e24edd0ed
jauresmanouan <jauresmanouan@gmail.com>
2024-06-10 22:35:13 +0000

    ♻️ F08 : ORDER > Add styling for <EmptyMenuAdmin/> and <EmpyMenuClient/>

--- src/components/pages/order/Main/Menu/EmptyMenuAdmin.jsx
+++ src/components/pages/order/Main/Menu/EmptyMenuAdmin.jsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
+import Button from "../../../../reusale-ui/Button";
+import { theme } from "../../../../../theme";
 
-export default function EmptyMenuAdmin({onReset}) {
+export default function EmptyMenuAdmin({ onReset }) {
   return (
     <EmptyMenuAdminStyled className="empty-menu-admin">
-      <span>Le menu est vide ?</span>
-      <span>Cliquez ci-dessous pour le réinitialiser</span>
-      <button onClick={onReset}>Générer de nouveaux produits</button>
+      <span className="first-text">Le menu est vide ?</span>
+      <span className="second-text">
+        Cliquez ci-dessous pour le réinitialiser
+      </span>
+      <Button
+        className="button"
+        label="Générer de nouveaux produits"
+        onClick={onReset}
+        version="primary"
+      />
     </EmptyMenuAdminStyled>
   );
 }
@@ -18,7 +27,22 @@ const EmptyMenuAdminStyled = styled.div`
   width: 100%;
   height: 100%;
 
-  button {
+  font-family: "Amatic SC";
+  font-size: ${theme.fonts.size.P4};
+  color: ${theme.colors.greyBlue};
+  .first-text {
+    font-weight: ${theme.fonts.weights.bold};
+  }
+  .second-text {
+    font-weight: ${theme.fonts.weights.regular};
+    margin-top: 21px;
+    margin-bottom: 31px;
+  }
+
+  .button {
     cursor: pointer;
+    height: 50px;
+    width: 225px;
+    font-size: ${theme.fonts.size.XS};
   }
 `;
--- src/components/pages/order/Main/Menu/EmptyMenuClient.jsx
+++ src/components/pages/order/Main/Menu/EmptyMenuClient.jsx
@@ -1,10 +1,11 @@
 import styled from "styled-components";
+import { theme } from "../../../../../theme";
 
 export default function EmptyMenuClient() {
   return (
     <EmptyMenuClientStyled className="empty-menu-client">
-      <span>Victime de notre succès ! :D</span>
-      <span>De nouvelles recettes sont en cours de préparation.</span>
+      <span className="first-text">Victime de notre succès ! :D</span>
+      <span className="second-text">De nouvelles recettes sont en cours de préparation.</span>
       <span>À très vite !</span>
     </EmptyMenuClientStyled>
   );
@@ -17,4 +18,16 @@ const EmptyMenuClientStyled = styled.div`
   flex-direction: column;
   width: 100%;
   height: 100%;
+
+  font-family: "Amatic SC";
+  font-size: ${theme.fonts.size.P4};
+  color: ${theme.colors.greyBlue};
+  .first-text {
+    font-weight: ${theme.fonts.weights.bold};
+  }
+  .second-text {
+    font-weight: ${theme.fonts.weights.regular};
+    margin-top: 21px;
+    margin-bottom: 21px;
+  }
 `;
